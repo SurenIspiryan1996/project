0156323d7fdebdc7a21c1095e3facb6b0c07ab1b
SurenIspiryan1996 <44082438+SurenIspiryan1996@users.noreply.github.com>
2019-01-18 17:40:08 +0300

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,5 +1,18 @@
-jQuery(document).ready(function ($) {
-	  var Slide = [
-                ]
+$(document).ready(function(){
+
+	$(".img").click(function(){
+    	$("#slider").show();
+    		$(".img").hide();
+	});
+
+	$("#slider").mouseenter(function(){
+		$(".btn").show();
+	});
+	$("#slider").mouseleave(function(){
+		$(".btn").hide();
+	});
+
 });
+
+
 
